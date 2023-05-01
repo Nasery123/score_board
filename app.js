70a399710816d56baf9d6df320d87b52440dcdef
Abdul Rauof Nasery <rauf.nasery143@gmail.com>
2023-05-01 15:36:16 -0600

    push

---
+++ app.js
@@ -0,0 +1,30 @@
+let homescore = 0;
+let awayscore = 0;
+
+let homeElement = document.getElementById('hscore');
+let awayElement = document.getElementById('ascore');
+//home.textContent = 0;
+//away.textContent = 0;
+
+function homescore1(){
+    homescore++;
+    drawScores()
+
+}
+function awayscore1(){
+    awayscore++;
+    drawScores()
+}
+function homescore3(){
+    homescore += 3;
+    drawScores()
+
+}
+function awayscore3(){
+    awayscore += 3;
+    drawScores()
+}
+function drawScores(){
+    homeElement.textContent = homescore;
+    awayElement.textContent = awayscore;
+}
